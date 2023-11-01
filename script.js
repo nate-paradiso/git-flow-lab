f33418bf537f3e0402c754041b27ce565a8898d4
Natasha Miller <miller.natasha@gmail.com>
2023-11-01 12:35:21 -0400

    Update poem object

--- script.js
+++ script.js
@@ -17,6 +17,11 @@ const poems = [
 
   // YOUR CODE HERE: Add an object containing two key value pairs: author and poem. Set the author property as equal to a string with your first name. Set the poem property as equal to a short "roses are red" poem. Use the new line character as in the example when you would like a line break.
 ];
+{
+  author: "Natasha";
+  poem: "Roses Are Read";
+  image: ""
+};
 
 let displayPoems = (array) => {
   let html = "";
